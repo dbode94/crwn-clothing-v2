461f17b61fc101dbad7436e88577929631b62c15
dbode94 <dbode94@gmail.com>
2023-11-07 16:57:39 -0500

    adding _redirect for netlify

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
-
-
 export const DirectoryContainer = styled.div`
     width: 100%;
     display: flex;
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,7 +4,7 @@ const FormInput = ({label, ...otherProps}) =>{
     return(
         <Group>
             <Input {...otherProps}/> {/*using the spread operator to fill upp all the properties we might need to use in the input element*/}
-            {label && <FormInputLabel className={`${otherProps.value.length? 'shrink' : ''} form-input-label`}>{label}</FormInputLabel>} {/* if label exist then render the label element - it is taking advantage of the condition evaluation order */}            
+            {label && <FormInputLabel shrink={otherProps.value.length} >{label}</FormInputLabel>} {/* if label exist then render the label element - it is taking advantage of the condition evaluation order */}            
         </Group>
     )
 }
