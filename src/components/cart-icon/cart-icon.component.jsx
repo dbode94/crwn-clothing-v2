345b1056ca1bfa6eb649501f25ac2162a0233c84
dbode94 <dbode94@gmail.com>
2024-01-12 15:45:43 -0500

    safe backup

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,4 @@
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+// import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -7,7 +7,6 @@ import { setIsCartOpen } from '../../store/cart/cart.action'
 
 import {CartIconContainer, ShoppingIconStyledComponent,ItemCount} from './cart-icon.style'
 
-import { useContext } from 'react'
 
 const CartIcon = () =>{
 
--- src/store/store.js
+++ src/store/store.js
@@ -3,21 +3,9 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
+import logger from "redux-logger";
+
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) =>{
-    if(!action.type){
-        return next(action);
-    }
-
-    console.log('type: ',action.type);
-    console.log('payload: ',action.payload)
-    console.log('currentState: ',store.getState())
-
-    next(action);
-
-    console.log('next state: ', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -27,10 +15,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware]
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean) // using the condition logic evaluation order to log only if we are in development, and since it is using a array, we can leverage the filter method to avoid passing 'false' if condition is not met
 
-const composeEnhances = compose(applyMiddleware(...middleWares));
+/*if you want your website to be Redux DevTool available, you need to modify your compose method -> make sure you are not in production,
+you have a window object and "__REDUX_DEVTOOLS_EXTENSION_COMPOSE__" tools exists (which is a protected keyword)*/
 
-export const store = createStore(persistedReducer, undefined, composeEnhances);
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
 
 export const persistor = persistStore(store);
